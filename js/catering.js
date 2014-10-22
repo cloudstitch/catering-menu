7304427b5603ba377b3835ac3f96801e23d38058
Ted Benson <eob@csail.mit.edu>
2014-10-22 18:31:39 -0400

    final

--- js/catering.js
+++ js/catering.js
@@ -1,83 +1,54 @@
 
-function generateData() {
+function showMenuFor(dateStr) {
   var itemList = CTS('sheet|Meals!rows')[0].toJson();
   var typeList = CTS('sheet|MealTypes!rows')[0].toJson();
 
   var ret = {};
+  var html = "";
 
   for (var i = 0; i < itemList.length; i++) {
     var meal = itemList[i];
-    var day = moment(meal.Date, 'MM/DD/YYYY').format('MM-DD-YYYY');
-    var html = "<ul class='menu'>";
-
-    for (var j = 0; j < typeList.length; j++) {
-      var type = typeList[j];
-      if (meal[type.ID]) {
-        var begin = type.Start;
-        var end = type.End;
-        var title = meal[type.ID];
-        html += "<div class='bs-callout bs-callout-" + type.ID + "'>";
-        html += "<h4>" + type.ID + "</h4>";
-        html += "<p>" + title + "</p>";
-        html += "</div>";
+    var day = moment(meal.Date, 'MM/DD/YYYY').format('MM/DD/YYYY');
+    console.log(day, dateStr)
+    if (day == dateStr) {
+      for (var j = 0; j < typeList.length; j++) {
+        var type = typeList[j];
+        if (meal[type.ID]) {
+          var begin = moment(type.Start, 'hh:mm:ss a').format('h:mm a');
+          var end = moment(type.End, 'hh:mm:ss a').format('h:mm a');
+          var title = meal[type.ID];
+          html += "<div class='bs-callout bs-callout-" + type.ID + "'>";
+          html += "<h4>" + type.ID + " ";
+          html += "<span>" + begin + " - " + end + "</span>";
+          html += "</h4>";
+          html += "<p>" + title + "</p>";
+          html += "</div>";
+        }
       }
     }
-    ret[day] = html;
   }
-  console.log(ret);
-  return ret;
+  if (html == "") {
+    html += "<div class='alert alert-info'>";
+    html += "<p>No meal data for this day.</p>";
+    html += "</div>";
+  }
+  $('#menu').html(html);
 
+  return ret;
 }
 
 $(function() {
-  window.cal = $('#calendar').calendario({
-    onDayClick: function( $el, $contentEl, dateProperties ) {
-      for(var key in dateProperties) {
-        console.log( key + ' = ' + dateProperties[ key ] );
-      }
-    }
-  });
-
-  var curMonth = cal.getMonthName();
-  var curYear = cal.getYear();
-
-  var $nextBtn = $('#custom-next');
-  var $prevBtn = $('#custom-prev');
-  var $todayBtn = $('#custom-current');
-
-  var $month = $('#custom-month');
-  var $year = $('#custom-year');
-  updateMonthYear();
-
-  $nextBtn.on( 'click', function() {
-    cal.gotoNextMonth( updateMonthYear );
-  });
-  $prevBtn.on( 'click', function() {
-    if ((cal.getMonthName() == curMonth) && (cal.getYear() == curYear)) {
-    } else {
-      cal.gotoPreviousMonth( updateMonthYear );      
-    }
-  });
-  $todayBtn.on( 'click', function() {
-    cal.gotoNow( updateMonthYear );
-  });
-
-  function updateMonthYear() {        
-    $month.html( cal.getMonthName() );
-    $year.html( cal.getYear() );
-
-    if ((cal.getMonthName() == curMonth) && (cal.getYear() == curYear)) {
-      $prevBtn.css({opacity: 0.3});
-      $todayBtn.css({opacity: 0.3});
-    } else {
-      $prevBtn.css({opacity: 1.0});
-      $todayBtn.css({opacity: 1.0});
-    }
-  }
-
   CTS.booted.then(
     function() {
-      window.cal.setData(generateData());
+      $('#paginator')
+        .datepaginator({
+          selectedDateFormat: 'MM/DD/YYYY'
+        })
+        .on('selectedDateChanged', function(event, date) {
+          var dateStr = date.format('MM/DD/YYYY');
+          showMenuFor(dateStr);
+        });
+      showMenuFor(moment().format('MM/DD/YYYY'));
     }
   );
 });
--- js/data.js
+++
@@ -1,7 +0,0 @@
-var codropsEvents = {
-	'09-05-2014' : [
-		'<span class="lunch">Hamburgers</span>',
-		'<span class="dinner">Hamburgers</span>',
-		'<span class="snack">Hamburgers</span>',
-	]
-};
